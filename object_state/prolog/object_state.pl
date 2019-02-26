03364ca99ac7a47cb5f2e8c276c92801dabeada8
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2019-02-26 14:58:16 +0100

    Add quick connection test predicate

--- object_state/prolog/object_state.pl
+++ object_state/prolog/object_state.pl
@@ -4,6 +4,7 @@
       create_object/2,
       object_at/4,
       test_belief/0,
+      spawn_on_table/0,
       clear/0,
       table_transform/0,
       object_at_table/1,
@@ -24,7 +25,8 @@
 			object_at(r,r,r,?),
 			object_at_table(?),
 			object_of_type(r,?),
-			test_belief.
+			test_belief,
+			spawn_on_table.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Interface predicates %%
@@ -51,11 +53,12 @@ object_at(ObjectType, Transform, Threshold, Instance):-
 
 
 
-
+spawn_on_table :-
+	belief_perceived_at(knowrob:'Cup', ['map', _, [1,0,0.8],[0,0,0,1]], 0.2, _).
 
 
 test_belief :-
-	belief_perceived_at(knowrob:'Cup', ['map', _, [-1,1,0.8],[0,0,0,1]], 0.2, _),
+	belief_perceived_at(knowrob:'Cup', ['map', _, [1,0,0.8],[0,0,0,1]], 0.2, _),
 	object_at_table(Instance),
 	object_of_type(knowrob:'Cup', Instances),
 	member(Instance,Instances),

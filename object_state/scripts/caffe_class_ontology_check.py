b8fc2e809ceb575437d17a3af708e29fab5a5926
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2019-03-19 17:42:48 +0100

    update caffe class checker with more reasonable output

--- object_state/scripts/caffe_class_ontology_check.py
+++ object_state/scripts/caffe_class_ontology_check.py
@@ -3,38 +3,43 @@
 import rospy
 import os
 import yaml
-import rospkg
+from rospkg import RosPack
 from json_prolog import json_prolog
 
-rospack = rospkg.RosPack()
+rospack = RosPack()
 prolog = json_prolog.Prolog()
 
 
-def transform_caffe_config_yaml(package='object_state', path='/example_perception.yaml'):
+def check_caffe_config_yaml(package='object_state', path='/example_perception.yaml'):
     rospy.loginfo("Waiting for json_prolog.")
     rospy.wait_for_service('/json_prolog/query')
     file_path = rospack.get_path(package) + path
-    caffe_splits_map = []
+    caffe_splits_names = []
 
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             content = file.read().replace('%YAML:1.0', '')
-            caffe_splits_map = yaml.load(content)
+            caffe_splits_names = yaml.load(content)
     else:
         rospy.logerr('File ' + path + ' in package ' + package + 'does not exist.')
 
-    output_yaml = dict()
-    output_yaml['classes'] = []
+    unresolved_classes = []
 
-    for c in caffe_splits_map['classes']:
-        query_string = "owl_description(hsr_objects:'" + c.capitalize().replace('_', '') + "',_)"
+    for c in caffe_splits_names['classes']:
+        owlised_name = c.capitalize().replace('_', '')
+        query_string = "owl_description(hsr_objects:'" + owlised_name + "',_)"
         solution = prolog.all_solutions(query_string)
         if not solution:
-            print(c.capitalize() + ' is missing')
-        else:
-            output_yaml['classes'].append(c)
+            unresolved_classes.append([c, owlised_name])
+
+    if unresolved_classes:
+        rospy.logwarn("Couldn't find classes:")
+        for cls in unresolved_classes:
+            rospy.logwarn(cls[0] + " translated to owl-class " + cls[1])
+    else:
+        rospy.loginfo("All classes found in ontology.")
 
 
 if __name__ == '__main__':
     rospy.init_node('caffe_config', anonymous=True)
-    transform_caffe_config_yaml()
+    check_caffe_config_yaml()
